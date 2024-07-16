b3a0424c845159d92ee93304be617449eab79a85
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-07-16 22:30:04 +0530

    chore: making as server component

--- src/app/components/ui/tfa/TFAProvider.tsx
+++ src/app/components/ui/tfa/TFAProvider.tsx
@@ -1,39 +1,27 @@
-'use client'
-
-import React, { useEffect } from 'react'
-
 import { checkOTPExpiration } from './CheckOTPExpiration'
 import './styles.css'
 
-const TFAProvider = (props: { children?: React.ReactNode }) => {
-  useEffect(() => {
-    console.log('main fta provider')
-    const getMe = async () => {
-      console.log('main fta provider in get me')
-      const res = await fetch(`/api/users/me`, {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
+const TFAProvider = async (props: { children?: React.ReactNode }) => {
+  const res = await fetch(`${process.env.PAYLOAD_URL}/api/users/me`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
 
-      const { user } = await res.json()
+  const { user } = await res.json()
 
-      if (user && window.location.pathname !== '/admin/tfa') {
-        console.log(user, 'from tfa')
-        if (
-          !user.otp?.otpVerified ||
-          !user.otp.otpSessionExpires ||
-          !checkOTPExpiration(user.otp.otpSessionExpires)
-        ) {
-          window.location.href = '/admin/tfa'
-        }
-      }
+  if (user && window.location.pathname !== '/admin/tfa') {
+    console.log(user, 'from tfa')
+    if (
+      !user.otp?.otpVerified ||
+      !user.otp.otpSessionExpires ||
+      !checkOTPExpiration(user.otp.otpSessionExpires)
+    ) {
+      window.location.href = '/admin/tfa'
     }
-
-    getMe()
-  }, [])
+  }
   return <main>{props.children}</main>
 }
 
